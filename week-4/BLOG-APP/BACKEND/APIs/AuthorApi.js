88939e34a90f9cfb75d1687f217b42906881be0e
Anandam Chattrapate <23eg105e05@anurag.edu.in>
2026-03-10 17:42:45 +0530

    Delete week-4 directory

--- week-4/BLOG-APP/BACKEND/APIs/AuthorApi.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-export const AuthorApp=express.Router()
-
-// Register author
-// authenticate Author
-// create article
-// read Articles of author
-// edit article
-// delete aritcles (soft/ hard)
--- week-4/BLOG-APP/BACKEND/APIs/UserApi.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-export const UserApp=express.Router()
-
-// create / register user
-// auth / login
-// read all articles
-// add comment to an article
